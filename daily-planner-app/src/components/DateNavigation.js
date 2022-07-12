99249c20bfb8684355cbd0efec9a5f3dd9475e16
RedaAlb <reda.alb2@hotmail.com>
2022-07-12 14:13:03 +0100

    Add date time picker navigation

--- daily-planner-app/src/components/DateNavigation.js
+++ daily-planner-app/src/components/DateNavigation.js
@@ -1,26 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { MobileDateTimePicker } from '@mui/x-date-pickers/MobileDateTimePicker';
+
+import TextField from '@mui/material/TextField';
 import { Fab } from "@mui/material";
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 
 
 function DateNavigation(props) {
+  const [currentDate, setCurrentDate] = useState(new Date());
+
+
+  const onDateChange = (date) => {
+    setCurrentDate(date);
+  }
+
+
   const onNextDayClick = () => {
-    console.log("Next day");
+    const nextDay = new Date(currentDate);
+    nextDay.setDate(nextDay.getDate() + 1);
+
+    setCurrentDate(nextDay);
   }
 
 
   const onPreviousDayClick = () => {
-    console.log("Previous day");
+    const prevDay = new Date(currentDate);
+    prevDay.setDate(prevDay.getDate() - 1);
+
+    setCurrentDate(prevDay);
   }
 
 
   return (
     <>
-      <div>
-        10 July 2022 10/7/22 Sunday 17:28
-      </div>
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <MobileDateTimePicker
+          label=""
+          inputFormat="dd MMMM yyyy | EEEE HH:mm"
+          ampm={false}
+          value={currentDate}
+          onChange={onDateChange}
+          renderInput={(params) =>
+            <TextField fullWidth  {...params} />
+          }
+        />
+      </LocalizationProvider>
 
       <Fab
         onClick={onNextDayClick}
